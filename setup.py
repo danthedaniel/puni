79ec3fb9fdd991f9ce6287a1ba577ce8617747ad
Daniel <teaearlgraycold@users.noreply.github.com>
2016-03-24 18:42:13 -0400

    setup.py and license changes

--- setup.py
+++ setup.py
@@ -3,8 +3,17 @@ from setuptools import setup
 setup(name='puni',
       version='0.2',
       description='Python UserNotes Interface for reddit',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities'
+      ],
       url='http://github.com/teaearlgraycold/puni',
       author='teaearlgraycold',
-      license='MIT',
+      license='GPLv3',
       packages=['puni'],
+      install_requires=[
+        'praw >=3.0.0',
+      ],
       zip_safe=False)
