837aabd528952d6d42af5816050b01bdce68a820
13steinj <13steinj@users.noreply.github.com>
2016-07-29 16:53:21 -0400

    Simplify usernotes
    Simplifies handling, makes exceptions in-line with PRAW intended audiences, and makes the version scheme follow PEP440 (after a bump to 0.3.3a1).
    
    PRAW (at sometime after 3.2) automatically handles the 50X errors that were manually handled before, and the retry times shouldn't be any more than 3 (the amount of times praw retries them for). Forbidden errors are impossible due to PRAW's restrict_access decorator, and reddit serving pages cached (even if by less than a second), so a lot of things can safely be ripped out.
    
    Constants shouldn't be defined at the instance-level, and the note warning constants were a tad long.
    
    There was some unneccessary recursion occuring in get_json, which loaded up the stack frame and memory usage, decreasing efficiency.

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
+from os import path
+from re import search
 from setuptools import setup
 
-setup(name='puni',
-      version='0.3.2',
+PACKAGE_NAME = "puni"
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, PACKAGE_NAME, 'version.py'),
+          encoding='utf-8') as fp:
+    VERSION = search("__version__ = '([^']+)'", fp.read()).group(1)
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
       description='Python UserNotes Interface for reddit',
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Utilities'
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Topic :: Utilities',
       ],
       url='http://github.com/teaearlgraycold/puni',
       author='teaearlgraycold',
       license='GPLv3',
-      packages=['puni'],
+      packages=[PACKAGE_NAME],
       install_requires=[
-        'praw >=3.2.0, <4.0',
+          'praw >=3.5.0, <4.0',
       ],tests_require = [
-        'nose',
+          'nose',
       ],
       test_suite="nose.collector",
-      zip_safe=False)
+      zip_safe=False
+)
