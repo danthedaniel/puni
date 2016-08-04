5e90e8d57ca8b9528a34dde0ed1167ed823f09b8
Daniel Angell <dpa34@drexel.edu>
2016-08-04 13:35:43 -0400

    Bump version to 0.3.2

--- puni/version.py
+++ puni/version.py
@@ -3,4 +3,4 @@ puni version definition
 """
 
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='puni',
-      version='0.3.1',
+      version='0.3.2',
       description='Python UserNotes Interface for reddit',
       classifiers=[
         'Development Status :: 3 - Alpha',
